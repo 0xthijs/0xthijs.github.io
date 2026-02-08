a43b3410b6b8c3d09a0e24c40695c724666bb223
0xthijs <thijsaa@gmail.com>
2026-02-08 13:44:41 +0700

    Update queries.sql
    Update 4: Added CASE statements for employee segmentation

--- queries.sql
+++ queries.sql
@@ -41,3 +41,36 @@ SELECT MaritalStatus, COUNT(*) as AttritionCount
 FROM employees
 WHERE Attrition = 'Yes'
 GROUP BY MaritalStatus;
+
+-- Week 4: Case implementations
+
+-- Query 1: Categorize employees by Age Group
+SELECT
+	Age,
+	CASE
+		WHEN Age < 27 THEN 'Gen Z'
+		WHEN Age BETWEEN 27 AND 42 THEN 'Millennial'
+		WHEN Age BETWEEN 43 AND 58 THEN 'Gen X'
+		ELSE 'Boomer'
+	END AS Generation_Group
+FROM employees;
+
+-- Query 2: Group employees into Salary Bands
+SELECT
+	MonthlyIncome,
+	CASE
+		WHEN MonthlyIncome < 5000 THEN 'Entry Level'
+		WHEN MonthlyIncome BETWEEN 5000 AND 15000 THEN 'Mid Level'
+		ELSE 'Executive'
+	END AS Salary_Band
+FROM employees;
+
+-- Query 3: Flag High Risk employees based on Job Satisfaction
+SELECT
+	Attrition,
+	JobSatisfaction,
+	CASE
+		WHEN JobSatisfaction <= 2 THEN 'High Risk'
+		ELSE 'Stable'
+	END AS Retention_Risk_Status
+FROM employees;
